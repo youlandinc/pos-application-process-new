a3310c9a1c161c93f5941eb382ac645c714f8a1a
lee <eric@youland.com>
2023-03-16 15:16:23 +0800

    build: first init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,37 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: false,
+  trailingSlash: true,
+  // i18n: {
+  //   locales: ['zh', 'en'],
+  //   defaultLocale: 'en',
+  //   localeDetection: false,
+  //   domains: [
+  //     {
+  //       domain: 'example.zh',
+  //       defaultLocale: 'zh',
+  //     },
+  //     {
+  //       domain: 'example.com',
+  //       defaultLocale: 'en',
+  //     },
+  //   ],
+  // },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+  // exportPathMap: async function (
+  //   defaultPathMap,
+  //   { dev, dir, outDir, distDir, buildId },
+  // ) {
+  //   return {
+  //     '/': { page: '/' },
+  //   };
+  // },
+};
