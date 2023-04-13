a5e09b114f779d1b9f787f650090c94229148748
lee <eric@youland.com>
2023-04-13 15:06:48 +0800

    feat: buffer

--- src/components/atoms/StyledSelect/StyledSelect.types.ts
+++ src/components/atoms/StyledSelect/StyledSelect.types.ts
@@ -1,7 +1,6 @@
 import { SelectProps, SxProps } from '@mui/material';
 
 export interface StyledSelectProps extends SelectProps {
-  id: string;
   validate?: undefined | string[];
   options: Option[];
   sxHelperText?: SxProps;
