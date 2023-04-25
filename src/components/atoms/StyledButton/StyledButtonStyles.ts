e883561a6d4c644bc17f71b2fd98401cdb5a2360
YoulandNeo <neo@youland.com>
2023-04-25 10:55:20 +0800

    fix: iconButton style

--- src/components/atoms/StyledButton/StyledButtonStyles.ts
+++ src/components/atoms/StyledButton/StyledButtonStyles.ts
@@ -2,11 +2,13 @@ import { POSFont } from '@/styles';
 import { SxProps } from '@mui/material';
 
 export const StyledButtonStyles: SxProps = {
-  ...POSFont(16, 600, 1.5),
-  textTransform: 'none',
-  padding: '15px 20px',
-  borderRadius: 2,
-  boxShadow: 'none',
+  '&.MuiButton-root': {
+    ...POSFont(16, 600, 1.5),
+    textTransform: 'none',
+    padding: '15px 20px',
+    borderRadius: 2,
+    boxShadow: 'none',
+  },
   '&:disabled': {
     color: 'info.main',
   },
@@ -16,7 +18,7 @@ export const StyledButtonStyles: SxProps = {
   '&.Mui-disabled': {
     borderColor: 'background.broder_disabled',
   },
-  '&.MuiButton-sizeSmall, &.MuiIconsButton-sizeSmall': {
+  '&.MuiButton-sizeSmall': {
     padding: '7px 12px',
     fontSize: 14,
   },
