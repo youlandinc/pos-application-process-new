600e47281aed0b3044d67d04ee5f443cb34b9c73
lee <eric@youland.com>
2023-07-17 13:45:20 +0800

    fix : fix issue

--- src/utils/FindSpecificVariable.ts
+++ src/utils/FindSpecificVariable.ts
@@ -3,6 +3,6 @@ import { VariableName, VariableValue } from '@/types';
 export const POSFindSpecificVariable = <T extends VariableValue = any>(
   name: VariableName,
   variables: Variable<T>[],
-): { name: string; type: VariableType; value: T } | undefined => {
+): Variable<T> | undefined => {
   return variables?.find((v) => v.name === name);
 };
