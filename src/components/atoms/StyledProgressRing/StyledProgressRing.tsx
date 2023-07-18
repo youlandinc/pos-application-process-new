2424279f1794b6ab41558494c1edbc88d7e1ac75
lee <eric@youland.com>
2023-07-18 10:51:04 +0800

    refactor: styledProgressRing

--- src/components/atoms/StyledProgressRing/StyledProgressRing.tsx
+++ src/components/atoms/StyledProgressRing/StyledProgressRing.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import { FC } from 'react';
 import { Typography } from '@mui/material';
 
 interface StyledProgressRingProps {
@@ -16,7 +16,7 @@ interface StyledProgressRingProps {
 }
 
 // Actual component
-export const StyledProgressRing = ({
+export const StyledProgressRing: FC<StyledProgressRingProps> = ({
   caps = 'round',
   lineWidth = 5,
   progressColor = '#36B37E',
@@ -26,7 +26,7 @@ export const StyledProgressRing = ({
   transitionDuration = 300,
   total = 12,
   current = 0,
-}: Props) => {
+}) => {
   const halfSize = size / 2;
   const radius = halfSize - lineWidth / 2;
   const circleLength = radius * 2 * Math.PI;
