0cd4a628c54e70a55139bc35dd9b62e08ea939a1
lee <eric@youland.com>
2023-08-29 10:24:16 +0800

    feat : add svg animation loading

--- src/utils/Handler.ts
+++ src/utils/Handler.ts
@@ -24,3 +24,26 @@ export const POSGetProductTypeByUrl = (val: string): string => {
 export const POSUpperFirstLetter = (val: string): string => {
   return val.charAt(0).toUpperCase() + val.slice(1);
 };
+
+export const POSSVGAnimation = (
+  delay: number,
+  duration: number,
+  strokeWidth: number,
+  timingFunction: string,
+  strokeColor: string,
+  repeat: boolean,
+) => {
+  const paths: NodeListOf<SVGPathElement> =
+    document.querySelectorAll('.loading-svg');
+  const mode = repeat ? 'infinite' : 'forwards';
+  for (let i = 0; i < paths.length; i++) {
+    const path = paths[i];
+    const length = path.getTotalLength();
+    path.style.strokeDashoffset = `${length}px`;
+    path.style.strokeDasharray = `${length}px`;
+    path.style.strokeWidth = `${strokeWidth}px`;
+    path.style.stroke = `${strokeColor}`;
+    path.style.animation = `${duration}s svg-text-anim ${mode} ${timingFunction}`;
+    path.style.animationDelay = `${i * delay}s`;
+  }
+};
