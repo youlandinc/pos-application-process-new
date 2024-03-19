381a3ebdfaa5f4be4b85dbac233ff770cb6b58d5
lee <eric@youland.com>
2024-03-19 16:40:00 +0800

    feat: replace content

--- src/components/organisms/Dashboard/Ground/Purchase/GroundPurchaseOverview.tsx
+++ src/components/organisms/Dashboard/Ground/Purchase/GroundPurchaseOverview.tsx
@@ -180,11 +180,14 @@ export const GroundPurchaseOverview: FC = observer(() => {
           ],
         });
         setProduct({
-          title: 'Rate',
+          title: 'Loan terms',
           subTitle: 'Interest rate',
           subInfo: POSFormatLocalPercent(product.interestRateOfYear),
           info: [
-            { label: 'Loan term', info: `${product.loanTerm} months` },
+            {
+              label: 'Loan duration',
+              info: `${product.loanTerm} months`,
+            },
             {
               label: 'Monthly payment',
               info: POSFormatDollar(product.paymentOfMonth),
