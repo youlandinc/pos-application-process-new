1be190504b8937763d87f8103d334c97e95295bf
lee <eric@youland.com>
2024-06-05 14:40:25 +0800

    feat: remove Redundant code

--- src/components/molecules/Account/Qualification/Agreement.tsx
+++ src/components/molecules/Account/Qualification/Agreement.tsx
@@ -9,7 +9,7 @@ import { observer } from 'mobx-react-lite';
 import { useMst } from '@/models/Root';
 
 import { useBreakpoints, useRenderPdf, useSwitch } from '@/hooks';
-import { AUTO_HIDE_DURATION, LOGIN_APP_KEY, userpool } from '@/constants';
+import { AUTO_HIDE_DURATION, userpool } from '@/constants';
 
 import {
   StyledButton,
@@ -27,7 +27,6 @@ import {
   _generateRoleFile,
   _previewRoleFile,
   _updateRoleTaskDetail,
-  _userRefreshToken,
 } from '@/requests';
 
 const initialDocumentFile = {
@@ -42,7 +41,7 @@ export const Agreement: FC = observer(() => {
   const breakpoints = useBreakpoints();
   const { enqueueSnackbar } = useSnackbar();
 
-  const { userType, session } = useMst();
+  const { userType } = useMst();
 
   const { visible, open, close } = useSwitch(false);
 
