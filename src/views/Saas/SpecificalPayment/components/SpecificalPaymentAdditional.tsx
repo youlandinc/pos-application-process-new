a2bc9f78d0af050f7892a1ba8643af6c9168d3c7
lee <eric@youland.com>
2024-06-28 09:48:08 +0800

    style: payment style

--- src/views/Saas/SpecificalPayment/components/SpecificalPaymentAdditional.tsx
+++ src/views/Saas/SpecificalPayment/components/SpecificalPaymentAdditional.tsx
@@ -5,7 +5,7 @@ export const SpecificalPaymentAdditional: FC<{ sx?: SxProps }> = ({
   ...rest
 }) => {
   return (
-    <Stack color={'#636A7C'} gap={{ xs: 1.5, md: 3 }} {...rest}>
+    <Stack color={'#636A7C'} gap={{ xs: 1.5, md: 3 }} ml={3} {...rest}>
       <Typography color={'#9095A3'} variant={'subtitle1'}>
         What happens next
       </Typography>
@@ -17,21 +17,21 @@ export const SpecificalPaymentAdditional: FC<{ sx?: SxProps }> = ({
         </Typography>
       </Stack>
 
-      {/*<Stack gap={1}>*/}
-      {/*  <Typography variant={'body3'}>Appraisal visit:</Typography>*/}
-      {/*  <Typography variant={'body3'}>*/}
-      {/*    An expert will come by to check your property&apos;s value, which may*/}
-      {/*    include looking inside your home.*/}
-      {/*  </Typography>*/}
-      {/*</Stack>*/}
+      <Stack gap={1}>
+        <Typography variant={'body3'}>Appraisal visit:</Typography>
+        <Typography variant={'body3'}>
+          An expert will come by to check your property&apos;s value, which may
+          include looking inside your home.
+        </Typography>
+      </Stack>
 
-      {/*<Stack gap={1}>*/}
-      {/*  <Typography variant={'body3'}>Heads up:</Typography>*/}
-      {/*  <Typography variant={'body3'}>*/}
-      {/*    Federal law says you need a carbon monoxide detector and a secure*/}
-      {/*    water heater. Make sure these are in place to avoid issues.*/}
-      {/*  </Typography>*/}
-      {/*</Stack>*/}
+      <Stack gap={1}>
+        <Typography variant={'body3'}>Heads up:</Typography>
+        <Typography variant={'body3'}>
+          Federal law says you need a carbon monoxide detector and a secure
+          water heater. Make sure these are in place to avoid issues.
+        </Typography>
+      </Stack>
     </Stack>
   );
 };
