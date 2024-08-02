6f95cca1e596b4edd6417b57b2583e4d2fd6aa73
lee <eric@youland.com>
2024-08-02 13:56:01 +0800

    style: test

--- src/components/atoms/StyledButtonGroup/StyledButtonGroupStyles.ts
+++ src/components/atoms/StyledButtonGroup/StyledButtonGroupStyles.ts
@@ -12,6 +12,14 @@ export const StyledButtonGroupStyles = {
     alignItems: 'center',
     justifyContent: 'center',
     p: 0,
+    '&.MuiToggleButtonGroup-middleButton, &.MuiToggleButtonGroup-lastButton': {
+      borderTopLeftRadius: '0px !important',
+      borderBottomLeftRadius: '0px !important',
+    },
+    '&.MuiToggleButtonGroup-middleButton, &.MuiToggleButtonGroup-firstButton': {
+      borderTopRightRadius: '0px !important',
+      borderBottomRightRadius: '0px !important',
+    },
     '&.MuiToggleButtonGroup-grouped:not(:first-of-type)': {
       ml: '-2px',
       borderLeft: '2px solid transparent',
