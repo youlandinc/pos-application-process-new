af396ff35cc1791e09d276e7c27ff82f670a2406
lee <eric@youland.com>
2024-11-20 16:39:56 +0800

    feat: logo

--- src/components/atoms/StyledHeaderLogo/StyledHeaderLogo.types.ts
+++ src/components/atoms/StyledHeaderLogo/StyledHeaderLogo.types.ts
@@ -1,6 +1,8 @@
 import { BoxProps } from '@mui/material';
+import { LayoutSceneTypeEnum } from '@/types';
 
 export interface StyledHeaderLogoProps extends BoxProps {
   logoUrl?: string;
   disabled?: boolean;
+  scene?: LayoutSceneTypeEnum;
 }
